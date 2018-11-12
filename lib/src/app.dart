a18b73a2903d52faf6cf9c6b7aea648458105889
Vijay <vijayganganapalli@gmail.com>
2018-11-12 08:34:32 +0530

    colors, floating action button added

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
+  final String title = "Well Being";
+  final barColor = const Color(0xff006064);
+  final bgColor = const Color(0xffffffff);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: title,
       home: Scaffold(
-        body: Center(
-          child: Text("Hello, World!"),
+        backgroundColor: bgColor,
+        appBar: AppBar(
+          backgroundColor: barColor,
+          title: Text(title),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: barColor,
+          child: Icon(Icons.add),
+          onPressed: () {},
         ),
       ),
     );
